483beda6f68ab1fbd513c1af013caac7a4b4d94c
l-aptop <116640288+l-aptop@users.noreply.github.com>
2022-12-03 09:54:47 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,106 @@
+from bleach.sanitizer import Cleaner
+from bleach.linkifier import Linker
+from sanic import Sanic
+from sanic.response import file
+connected = []
+cleaner = Cleaner(tags=[])
+app = Sanic('chat')
+bot = 'Chat Bot'
+run_ip = "127.0.0.1"
+port = 8080
+
+
+def open_in_new_tab(attrs, *args):
+    attrs[(None, 'target')] = '_blank'
+    return attrs
+
+
+linker = Linker(callbacks=[open_in_new_tab])
+
+
+def clean(text):
+    return cleaner.clean(text)
+
+
+def transform(text):
+    return linker.linkify(clean(text.strip()))
+
+
+def lstrip(text):
+    return text.lower().strip()
+
+
+async def broadcast(content):
+    for user in connected:
+        await user.send(content)
+
+
+@app.route('/')
+async def main(request):
+    return await file('index.html')
+
+
+@app.route('/script.js')
+async def script(request):
+    return await file('script.js')
+
+
+@app.route('/style.css')
+async def style(request):
+    return await file('style.css')
+
+
+@app.websocket('/socket')
+async def chat(request, ws):
+    username = request.args.get('username', 'None').strip()
+    username_l = username.lower()
+    if username_l in ('system', bot.lower()) or username_l == "" or len(username) > 12:
+        print('[INFO] Failed connection attempt: Username not allowed (username: {})'.format(username))
+        return await ws.send('{"error": "Username not allowed.", "code": 0}')
+    for user in connected:
+        if lstrip(user.username) == username_l:
+            print("[INFO] Failed connection attempt: Username already connected (username: {})".format(username))
+            return await ws.send('{"error": "Someone with that username is already connected.", "code": 0}')
+    username = clean(username).replace('\\', '\\\\')
+    ws.username = username
+    connected.append(ws)
+    await ws.send(
+        '{"username": "System", "message": "Connected to chat successfully!<br>"'
+        'There are AMOUNT people online.<br><br>'
+        'To view available Chat Bot commands, say !help"}'.replace('AMOUNT', str(len(connected)), 1)
+    )
+    print('[INFO] {} connected'.format(username))
+    await broadcast('{"username": "System", "message": "<b>' + username + '</b> has joined the chat."}')
+    try:
+        async for msg in ws:
+            msg = transform(msg).replace('\\', '\\\\').replace('"', '\\"').replace('\n', '<br>')
+            if msg != "" and len(msg) < 500:
+                print('{}: {}'.format(username, msg))
+                if msg.startswith('!'):
+                    if msg == '!help':
+                        await ws.send('{"username": "' + username + '", "message": "' + msg + '"}')
+                        await ws.send('{"username": "' + bot + '", "message": "<br>Hi <b>' + username + '</b>! My commands:<br>!online - shows everyone currently online"}')
+                    elif msg == "!online":
+                        await ws.send('{"username": "' + username + '", "message": "' + msg + '"}')
+                        online = ""
+                        for user in connected:
+                            online += user.username + '<br>'
+                        await ws.send('{"username": "' + bot + '", "message": "<br>Online people:<br>' + online + '"}')
+                    else:
+                        await ws.send('{"username": "' + username + '", "message": "' + msg + '"}')
+                        await ws.send(
+                            '{"username": "' + bot + '", "message": '
+                            '"<br>Command not found! You can view my commands by using !help '
+                            '(and also, all messages with a ! at the start are commands)"}'
+                        )
+                else:
+                    await broadcast('{"username": "' + username + '", "message": "' + msg + '"}')
+    except Exception as e:
+        print('[ERROR] {}: {}'.format(type(e), str(e)))
+    finally:
+        connected.remove(ws)
+        print('[INFO] {} disconnected'.format(username))
+        return await broadcast('{"username": "System", "message": "<b>' + username + '</b> has left the chat."}')
+
+if __name__ == "__main__":
+    app.run(host=run_ip, port=port, access_log=False)
